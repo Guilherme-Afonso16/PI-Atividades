45674812573c47ea96cd19ec1a2f4cc20f93c623
GUILHERME AFONSO SÉRGIO AVELINO <20211204010023@ifrn.local>
2025-10-02 10:27:47 -0300

    Criando método de Salvar e Confirmar

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -3,6 +3,8 @@ package ifrn.pi.eventos.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ifrn.pi.eventos.models.Evento;
+
 @Controller
 public class EventosController {
 
@@ -11,5 +13,14 @@ public class EventosController {
 		return "formEvento";
 	}
 	
+	@RequestMapping("/eventos/conf")
+	public String salvar(Evento evento) {
+		System.out.println("ABADABADUUUUUUUU");
+		System.out.println(evento.getNome());
+		System.out.println(evento.getLocal());
+		System.out.println(evento.getData());
+		System.out.println(evento.getHorario());
+		return "confirmar";
+	}
 }
 	
