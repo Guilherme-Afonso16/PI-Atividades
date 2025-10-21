495badb1c62d8fa722040380960f2261bccf0813
GUILHERME AFONSO SÉRGIO AVELINO <20211204010023@ifrn.local>
2025-10-21 15:33:27 -0300

    Listando convidados em detalhes

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -60,6 +60,10 @@ public class EventosController {
 		Evento evento = opt.get();
 
 		md.addObject("evento", evento);
+		
+		List<Convidado> convidados = cr.findByEvento(evento);	
+		md.addObject("convidados", convidados);
+		
 		return md; 
 	}
 	
